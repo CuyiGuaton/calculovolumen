2d15455604e8f0fcf9c34a911f13a2b9b0b850a9
Sergio Salinas <18431448+ssalinasf@users.noreply.github.com>
2020-10-22 14:41:51 -0300

    Agregado formula cuatro troncos

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 // Empty JS for your own code to be here
 
 var pi = 3.14159265359;
+
+function CalculoVolumenCilindro(radio,altura){
+    return radio*radio*altura*pi;
+}
+
+function CalculoVolumenTronco(radio,radio2,altura){
+    return pi*altura*(radio*radio + radio2*radio2 + radio*radio2)/3;
+}
+
 function volumenCilindro(){
     var radio =  document.getElementById("radiocilindro").value;
     var altura =  document.getElementById("alturacilindro").value;
     console.log(radio, altura);
-    var result  = radio*radio*altura*pi*0.001;
+    var result  = CalculoVolumenCilindro(radio,altura)*0.001;
     result2 = result.toFixed(2);
     document.getElementById("resultadoCilindro").textContent = result2.toString() + " Litros";
 }
@@ -15,7 +24,7 @@ function volumenTronco(){
     var radio =  document.getElementById("radio1tronco").value;
     var radio2 =  document.getElementById("radio2tronco").value;
     var altura =  document.getElementById("alturatronco").value;
-    result = ((pi*altura*(radio*radio + radio2*radio2 + radio*radio2)/3))*0.001;
+    result = CalculoVolumenTronco(radio, radio2, altura)*0.001;
     result2 = result.toFixed(2);
     document.getElementById("resultadotronco").textContent = result2.toString() + " Litros";
 }
@@ -26,8 +35,9 @@ function volumendosTronco(){
     var radio3 = document.getElementById("radio3dostronco").value;
     var altura1 =  document.getElementById("altura1dostronco").value;
     var altura2 =  document.getElementById("altura2dostronco").value;
-    var tronco1  = (pi*altura1*(radio*radio + radio2*radio2 + radio*radio2)/3);
-    var tronco2  = (pi*altura2*(radio3*radio3 + radio2*radio2 + radio2*radio3)/3);
+    //var tronco1  = (pi*altura1*(radio*radio + radio2*radio2 + radio*radio2)/3);
+    var tronco1  = CalculoVolumenTronco(radio, radio2, altura1);
+    var tronco2  = CalculoVolumenTronco(radio2, radio3, altura2);
     result = (tronco1 + tronco2)*0.001;
     result2 = result.toFixed(2);
     document.getElementById("resultadodostronco").textContent = result2.toString() + " Litros";
@@ -41,11 +51,31 @@ function volumendosTroncocilindro(){
     var altura2 =  document.getElementById("altura2dostroncocilindro").value;
     var altura3 =  document.getElementById("altura3dostroncocilindro").value;
     var tronco1  = (pi*altura1*(radio*radio + radio2*radio2 + radio*radio2)/3);
-    var cilindro = radio2*radio2*altura2*pi;
-    var tronco2  = (pi*altura3*(radio3*radio3 + radio2*radio2 + radio2*radio3)/3);
+    var tronco1  = CalculoVolumenTronco(radio, radio2, altura1);
+    var cilindro  = CalculoVolumenCilindro(radio2,altura2)
+    var tronco2  = CalculoVolumenTronco(radio2, radio3, altura3);
     result = (tronco1 + tronco2 + cilindro)*0.001;
     result2 = result.toFixed(2);
     document.getElementById("resultadodostroncocilindro").textContent = result2.toString() + " Litros";
+}
+
+function volumencuatroTroncos(){
+    var radio1 =  document.getElementById("radio1cuatrotronco").value;
+    var radio2 = document.getElementById("radio2cuatrotronco").value;
+    var radio3 = document.getElementById("radio3cuatrotronco").value;
+    var radio4 = document.getElementById("radio4cuatrotronco").value;
+    var radio5 = document.getElementById("radio5cuatrotronco").value;
+    var altura1 =  document.getElementById("altura1cuatrotronco").value;
+    var altura2 =  document.getElementById("altura2cuatrotronco").value;
+    var altura3 =  document.getElementById("altura3cuatrotronco").value;
+    var altura4 =  document.getElementById("altura4cuatrotronco").value;
+    var tronco1  = CalculoVolumenTronco(radio1, radio2, altura1);
+    var tronco2  = CalculoVolumenTronco(radio2, radio3, altura2);
+    var tronco3  = CalculoVolumenTronco(radio3, radio4, altura3);
+    var tronco4  = CalculoVolumenTronco(radio4, radio5, altura4);
+    result = (tronco1 + tronco2 + tronco3 + tronco4)*0.001;
+    result2 = result.toFixed(2);
+    document.getElementById("resultadocuatrotronco").textContent = result2.toString() + " Litros";
 }
 
 function calculoradio(){
